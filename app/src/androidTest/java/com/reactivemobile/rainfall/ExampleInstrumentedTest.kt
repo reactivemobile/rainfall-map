feff979ba7f3136052cb17dc842f7b8ff5a807c3
reactivemobile <donal@reactivemobile.com>
2020-11-25 18:55:47 +0000

    Renaming project

--- app/src/androidTest/java/com/reactivemobile/rainfall/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/reactivemobile/rainfall/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.reactivemobile.exchangerates", appContext.packageName)
+        assertEquals("com.reactivemobile.rainfallMap", appContext.packageName)
     }
 }
